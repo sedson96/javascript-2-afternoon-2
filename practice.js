9bf59dd0742946d90994003514672837e458f94c
Edson Silva-Filho <edsonsilva@Edsons-MacBook-Pro.local>
2019-03-13 17:29:28 -0500

    first

--- practice.js
+++ practice.js
@@ -19,7 +19,9 @@ var arr = [10,20,30];
 
 //Code Here
 
-
+function first(arr){
+  return arr[0];
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -34,7 +36,9 @@ var arr = [40,50,60];
 
 //Code Here
 
-
+function last(arr){
+  return arr[arr.length - 1];
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -49,12 +53,16 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family){
+  for (let i = 0; i < family.length;i++){
+    alert(family[i]);
+  }
+}
 
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
-var letters = ['A', 'B', 'C', 'D', 'E'];
+let letters = ['A', 'B', 'C', 'D', 'E'];
 // Do not edit the code above.
 
 /*
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(let i = letters.length -1 ; i >= 0; i--){
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -79,7 +91,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
-
+function evenFinder(nums){
+  let evens = [];
+  for(let i = 0; i < nums.length;i++){
+    let even = nums[i]
+    if (even % 2 === 0){
+      evens.push(even)
+    }
+  }
+  return evens;
+}
 
 
 
